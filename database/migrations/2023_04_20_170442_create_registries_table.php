66480785a9566e38bedad3595d1e77b6ffa4f34f
GuglielmoBini <guglielmobini@gmail.com>
2023-04-20 19:38:18 +0200

    migrated registries table

--- database/migrations/2023_04_20_170442_create_registries_table.php
+++ database/migrations/2023_04_20_170442_create_registries_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('registries', function (Blueprint $table) {
             $table->id();
+            $table->string('business_name', 100);
+            $table->string('status', 20);
+            $table->string('sector', 50);
+            $table->string('vat_number', 11)->unique();
+            $table->string('tax_id_code', 16);
+            $table->date('activity_start_date');
+            $table->char('rating', 1);
+            $table->string('chamber_of_commerce')->nullable();
+            $table->text('notes');
+            $table->string('email', 70)->unique();
+            $table->string('phone_number', 20);
+            $table->string('username', 50);
+            $table->string('password', 30);
             $table->timestamps();
         });
     }
