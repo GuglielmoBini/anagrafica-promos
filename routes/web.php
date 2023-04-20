7efde0ef3aeb2c58dcf9b15c1f2f24b87ab4692f
GuglielmoBini <guglielmobini@gmail.com>
2023-04-20 19:10:53 +0200

    created Registry controller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\RegistryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,10 +23,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//Route for Registry
+Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
+    // Code below groups all the CRUD's routes
+    Route::resource('registries', RegistryController::class);
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
