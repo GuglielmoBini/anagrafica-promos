ab025922ce466507d43eade79f4b8ad7361cc029
GuglielmoBini <guglielmobini@gmail.com>
2023-04-24 11:55:15 +0200

    seeded registry table

---
+++ config/aziende.php
@@ -0,0 +1,48 @@
+<?php
+return [
+    [
+        'business_name' => 'Byness s.r.l',
+        'status' => 'Business',
+        'sector' => 'salute',
+        'vat_number' => '12345678901',
+        'tax_id_code' => '1234567890987654',
+        'activity_start_date' => '1965-10-14',
+        'rating' => 'A',
+        'chamber_of_commerce' => 'upload/visura-catastale-1.pdf',
+        'notes' => 'Azienda che produce carta igienica e scottex',
+        'email' => 'byness@byness.it',
+        'phone_number' => '0572910705',
+        'username' => 'Byness65',
+        'password' => 'password'
+    ],
+    [
+        'business_name' => 'Pieraccini s.r.l',
+        'status' => 'Business',
+        'sector' => 'informatica',
+        'vat_number' => '12345678902',
+        'tax_id_code' => '1234567890987653',
+        'activity_start_date' => '1992-04-18',
+        'rating' => 'B',
+        'chamber_of_commerce' => 'upload/visura-catastale-2.pdf',
+        'notes' => 'Negozio di informatiza e tecnologia',
+        'email' => 'pieraccini@pieraccini.it',
+        'phone_number' => '0572910704',
+        'username' => 'Pieraccini92',
+        'password' => 'password'
+    ],
+    [
+        'business_name' => 'MHS s.a.s',
+        'status' => 'Business',
+        'sector' => 'salute',
+        'vat_number' => '12345678903',
+        'tax_id_code' => '1234567890987652',
+        'activity_start_date' => '1985-05-15',
+        'rating' => 'C',
+        'chamber_of_commerce' => 'upload/visura-catastale-3.pdf',
+        'notes' => 'Parrucchiere uomo/donna',
+        'email' => 'mhs@mhs.it',
+        'phone_number' => '0572910703',
+        'username' => 'Mhs85',
+        'password' => 'password'
+    ],
+];
