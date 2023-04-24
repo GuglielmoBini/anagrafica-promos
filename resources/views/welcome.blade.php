1306047ba207e5c8025972a30fe84c1fccbb4389
GuglielmoBini <guglielmobini@gmail.com>
2023-04-24 15:43:57 +0200

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,14 @@
 <div class="row justify-content-center align-items-center">
     <div class="col-10 text-center">
         <h1 class="my-5">Benvenuto</h1>
-        <h3 class="mb-5">Premi il pulsante sottostante per essere indirizzato alla nostra lista aziende</h3>
-        <a class="btn btn-primary" href="{{ route('admin.registries.index') }}">{{ __('VAI ALLE AZIENDE') }}</a>
+        @guest
+            <h3 class="mb-5">Effettua il Login</h3>
+            <a class="btn btn-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+        @endguest
+        @auth    
+            <h3 class="mb-5">Premi il pulsante sottostante per essere indirizzato alla nostra lista aziende</h3>
+            <a class="btn btn-primary" href="{{ route('admin.registries.index') }}">{{ __('VAI ALLE AZIENDE') }}</a>
+        @endauth
     </div>
 </div>
 @endsection
